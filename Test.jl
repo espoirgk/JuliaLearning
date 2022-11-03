add6e54d7b4db04e7a19729ee57512eac4ca8b34
Espoir <58880576+Espipi@users.noreply.github.com>
2022-11-03 23:21:12 +0100

    Create a test file

---
+++ Test.jl
@@ -0,0 +1,8 @@
+amount_of_rain = 0.
+if amount_of_rain == 0    
+    println("There is zero rain")
+elseif amount_of_rain < 1# Add a second condition    
+    println("Better get your coat")
+else    
+    println("That's a lot of rain, stay home")
+end
